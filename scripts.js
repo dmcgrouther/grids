b4ebe53bf713bc6d858d8f27a5c22405503a8a44
david-mcgrouther <dbmcgrouther@gmail.com>
2021-05-18 10:32:50 -0700

    added dropdown for columns.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 const addBoxButton = document.getElementById("addBoxButton");
 const createGridButton = document.getElementById("createGrid");
+const columnSelectButton = document.getElementById("column-select");
+
+let columnCount = 0;
 
 const addBox = () => {
   console.log('add box pressed');
@@ -9,5 +12,13 @@ const createGrid = () => {
   console.log('create grid pressed!');
 }
 
+function selectColumns(event){
+  var eID = document.getElementById("columns");
+
+  var columnCount = eID.value;
+  console.log(columnCount);
+}
+
 addBoxButton.addEventListener("click", addBox);
 createGridButton.addEventListener("click", createGrid);
+columnSelectButton.addEventListener("click", selectColumns)
---
+++ sources.md
@@ -0,0 +1,9 @@
+utlized this for dropdown list
+
+<!-- https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_js_dropdown -->
+
+
+<!-- https://www.w3schools.com/bootstrap/tryit.asp?filename=trybs_ref_comp_dropdown-menu&stacked=h -->
+
+
+https://stackoverflow.com/questions/1085801/get-selected-value-in-dropdown-list-using-javascript
