7a0d6e8554681b2c8ed83ab0215292a97e5eb305
david-mcgrouther <dbmcgrouther@gmail.com>
2021-05-18 16:55:28 -0700

    this commit makes it so when you remove a box, the numbers increment with the lost box. it also keeps the button

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ const boxRemoveButton = document.getElementsByClassName("remove");
 
 let columnCount = 1;
 let rowCount = 1;
+let containerLength = 1;
 
 function selectColumns(){
   var eID = document.getElementById("columns");
@@ -26,14 +27,36 @@ const addBox = () => {
   console.log('add box pressed');
 }
 
-const removeBox = () => {
+const removeBox = (event) => {
   console.log('remove box button pressed');
   event.target.parentElement.remove();
+  event.preventDefault();
+  // containerLength--;
+  // console.log(containerLength)
+  // let z = 1;
+
+  let containerChildren = container.children
+  // while(z <= containerChildren.length){
+  //   console.log()
+  // }
+  for(let z = 0; z < containerChildren.length; z++){
+    console.log(z)
+    containerChildren[z].innerHTML=z+1
+    let btn = document.createElement("button");
+    btn.innerHTML = "x";
+    btn.type = "remove";
+    btn.name = "remove";
+    btn.className+="remove";
+    containerChildren[z].id=z+1;
+    containerChildren[z].appendChild(btn);
+    btn.addEventListener("click", removeBox)
+  }
 }
 
 function createGrid() {
   container.style.setProperty('--grid-rows', rowCount);
   container.style.setProperty('--grid-cols', columnCount);
+  containerLength = rowCount * columnCount;
   for (c = 0; c < (rowCount * columnCount); c++) {
     let cell = document.createElement("div");
     cell.innerText = (c + 1);
@@ -56,4 +79,4 @@ addBoxButton.addEventListener("click", addBox);
 createGridButton.addEventListener("click", createGrid);
 columnSelectButton.addEventListener("click", selectColumns);
 rowSelectButton.addEventListener("click", selectRows);
-boxRemoveButton.addEventListener("click", removeBox)
+// boxRemoveButton.addEventListener("click", removeBox)
