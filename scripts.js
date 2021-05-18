5153afe04a5ce3623aab974a80c8d7d1b2f47883
david-mcgrouther <dbmcgrouther@gmail.com>
2021-05-18 13:00:54 -0700

    added function and event listener for rows

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
 const addBoxButton = document.getElementById("addBoxButton");
 const createGridButton = document.getElementById("createGrid");
 const columnSelectButton = document.getElementById("column-select");
+const rowSelectButton = document.getElementById("row-select");
+
 
 let columnCount = 0;
+let rowCount = 0;
 
 const addBox = () => {
   console.log('add box pressed');
@@ -16,9 +19,17 @@ function selectColumns(event){
   var eID = document.getElementById("columns");
 
   var columnCount = eID.value;
-  console.log(columnCount);
+  console.log(`columnCount is ${columnCount}`);
+}
+
+function selectRows(event){
+  var eID = document.getElementById("rows");
+
+  var rowCount = eID.value;
+  console.log(`rowCount is ${rowCount}`);
 }
 
 addBoxButton.addEventListener("click", addBox);
 createGridButton.addEventListener("click", createGrid);
-columnSelectButton.addEventListener("click", selectColumns)
+columnSelectButton.addEventListener("click", selectColumns);
+rowSelectButton.addEventListener("click", selectRows);
